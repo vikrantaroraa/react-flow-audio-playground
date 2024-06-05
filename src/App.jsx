3f004e37770b211e14deb190c1c562215764318a
vikrantaroraa <arora.veshu@gmail.com>
2024-06-05 23:03:51 +0530

    feat: created the custom node Osc and added it to react flow, also added twind for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import ReactFlow, { Background } from "reactflow";
 import "./App.css";
+import { shallow } from "zustand/shallow";
+
+import { useStore } from "./store";
+
+import Osc from "./nodes/Osc";
+
+const selector = (store) => ({
+  nodes: store.nodes,
+  edges: store.edges,
+  onNodesChange: store.onNodesChange,
+  onEdgesChange: store.onEdgesChange,
+  addEdge: store.addEdge,
+});
+
+const nodeTypes = {
+  osc: Osc,
+};
 
 function App() {
+  const store = useStore(selector, shallow);
   return (
-    <ReactFlow>
+    <ReactFlow
+      nodes={store.nodes}
+      edges={store.edges}
+      nodeTypes={nodeTypes}
+      onNodesChange={store.onNodesChange}
+      onEdgesChange={store.onEdgesChange}
+      onConnect={store.addEdge}
+    >
       <Background />
     </ReactFlow>
   );
--- src/store.js
+++ src/store.js
@@ -3,7 +3,16 @@ import { nanoid } from "nanoid";
 import { create } from "zustand";
 
 export const useStore = create((set, get) => ({
-  nodes: [],
+  nodes: [
+    {
+      type: "osc",
+      id: "a",
+      data: { frequency: 220, type: "square" },
+      position: { x: 0, y: 0 },
+    },
+    { id: "b", data: { label: "gain" }, position: { x: 50, y: 50 } },
+    { id: "c", data: { label: "output" }, position: { x: -50, y: 100 } },
+  ],
   edges: [],
 
   onNodesChange(changes) {
@@ -23,5 +32,13 @@ export const useStore = create((set, get) => ({
     const edge = { id, ...data };
 
     set({ edges: [edge, ...get().edges] });
+  },
+
+  updateNode(id, data) {
+    set({
+      nodes: get().nodes.map((node) =>
+        node.id === id ? { ...node, data: { ...node.data, ...data } } : node
+      ),
+    });
   },
 }));
